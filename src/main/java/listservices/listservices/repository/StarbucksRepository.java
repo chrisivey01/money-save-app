3821b4d8992150b3839228e3467b7d706358e336
Chris Ivey <chrisivey01@gmail.com>
2019-02-01 09:28:03 -0600

    repo added

--- src/main/java/listservices/listservices/repository/StarbucksRepository.java
+++ src/main/java/listservices/listservices/repository/StarbucksRepository.java
@@ -2,7 +2,9 @@ package listservices.listservices.repository;
 
 import listservices.listservices.entity.Starbucks;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StarbucksRepository extends CrudRepository<Starbucks, Integer> {
 
 }
